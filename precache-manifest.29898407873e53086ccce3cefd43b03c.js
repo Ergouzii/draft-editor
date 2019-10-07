9574775e833f8d0e51fabe4a7d7b9ed43ad93888
Xiaohui Liu <jayliu925@gmail.com>
2019-10-06 20:52:00 -0600

    Updates

---
+++ precache-manifest.29898407873e53086ccce3cefd43b03c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c192b9842f9c770ce946332792eb1763",
+    "url": "/draft-editor/index.html"
+  },
+  {
+    "revision": "49065630ecf9a2eaeeb7",
+    "url": "/draft-editor/static/css/main.1328c8b5.chunk.css"
+  },
+  {
+    "revision": "e0c7ad7bce9014b330e3",
+    "url": "/draft-editor/static/js/2.08beeada.chunk.js"
+  },
+  {
+    "revision": "49065630ecf9a2eaeeb7",
+    "url": "/draft-editor/static/js/main.fef232c1.chunk.js"
+  },
+  {
+    "revision": "8bd4f7cf8b49fabe663d",
+    "url": "/draft-editor/static/js/runtime-main.198a5969.js"
+  }
+]);
